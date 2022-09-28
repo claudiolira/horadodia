0b4dba486625eca4cd5e463917b519f45b9323bd
Claudio Lira <claudio.lira@linepay.com.br>
2022-09-28 16:32:08 -0300

    Alteração na apresentação da hora.

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
-    var min = data.getMinutes()
-    msg.innerHTML = `Agora são ${hora}h${min}.`
+    var minuto = data.getMinutes()
+    var segundo = data.getSeconds()
+    msg.innerHTML = `Agora são ${hora}h${minuto}min${segundo}s`
     if (hora >= 0 && hora < 12) {
         //Bom dia!
         img.src = 'fotoManha.jpg'
@@ -19,3 +20,7 @@ function carregar() {
         document.body.style.background = '#515154'
     }
 }
+
+setInterval(() => {
+    carregar()
+}, 1000);
